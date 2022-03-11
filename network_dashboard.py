d1505937420741c134267868dd075809b81dbe5a
Usman Abdulganiy Babatunde <ausman@aimsammi.com>
2022-03-11 12:19:01 +0100

    added piechart

--- network_dashboard.py
+++ network_dashboard.py
@@ -1,3 +1,4 @@
+from turtle import title
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -42,8 +43,11 @@ def load_device_data(device):
 
     device_name_data = device_uptime_count.join(
         device_downtime_count, how='outer').fillna(0).astype(np.int32)
-    device_name_data = device_name_data.add_suffix('_count')
+    #device_name_data = device_name_data.add_suffix('_count')
     device_name_data.index = pd.DatetimeIndex(device_name_data.index)
+    device_name_data['pct_uptime'] = (
+        device_name_data['uptime']/device_name_data.sum(axis=1)*100).round(1)
+    device_name_data['pct_downtime'] = 100 - device_name_data['pct_uptime']
 
     return device_name_data
 
@@ -61,20 +65,44 @@ device_name_data_month_wrap = dict(
     zip(device_name_data_month_name, device_name_data_month))
 selected_month = st.sidebar.radio('Month', device_name_data_month_name)
 
+# Barchart
 my_color = ['g', 'r']
-fig, axes = plt.subplots(1, 1, figsize=(14, 10))
+fig_bar, axes_bar = plt.subplots(1, 1, figsize=(14, 10))
 device_name_data[(device_name_data.index.year == selected_year) & (
-    device_name_data.index.month == device_name_data_month_wrap[selected_month])].plot(kind='bar', ax=axes, color=my_color)
-axes.yaxis.set_major_locator(MaxNLocator(integer=True))
-axes.set_title(
-    f'{selected_device} device network uptime and downtime count for the month of {selected_month}, {selected_year}')
+    device_name_data.index.month == device_name_data_month_wrap[selected_month])][['uptime', 'downtime']].plot(kind='bar', ax=axes_bar, color=my_color)
+axes_bar.yaxis.set_major_locator(MaxNLocator(integer=True))
+axes_bar.set_title(
+    f'{selected_device} device network uptime and downtime daily count for the month of {selected_month}, {selected_year}', fontsize=20)
 plt.tight_layout(pad=5)
 
+# Piechart
+
+device_name_data_months_group = device_name_data.groupby(
+    pd.Grouper(freq='M')).sum()[['uptime', 'downtime']]
+device_name_data_months_pct = device_name_data_months_group.div(
+    device_name_data_months_group.sum(axis=1), axis='rows').round(2)
+fig_pie, axes_pie = plt.subplots(1, 1, figsize=(10, 8))
+axes_pie.set_title(
+    f'{selected_device} device network uptime and downtime percent for the month of {selected_month}, {selected_year}', fontsize=15)
+device_name_data_month_pct = device_name_data_months_pct[(device_name_data_months_pct.index.year == selected_year) & (
+    device_name_data_months_pct.index.month == device_name_data_month_wrap[selected_month])]
+
+device_name_data_month_pct.T.plot.pie(
+    y=device_name_data_month_pct.index[0], colors=['green', 'red'], autopct='%0.1f%%', ax=axes_pie)
+axes_pie.yaxis.set_visible(False)
+
+
 st.write("Daily Uptime and Downtime count for", selected_device, "device")
-st.dataframe(device_name_data, height=200, width=500)
+st.dataframe(device_name_data, height=200, width=800)
 st.title('#')
 st.title('#')
-st.pyplot(fig)
+
+row_two_col = st.columns(2)
+with row_two_col[0]:
+    st.pyplot(fig_bar)
+
+with row_two_col[1]:
+    st.pyplot(fig_pie)
 
 hide_streamlit_style = """
             <style>
